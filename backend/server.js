9d19e22ac4faa68958eeb63952fac56f15c4acac
praveen gowda s r <praveengowdasr998@gmail.com>
2026-02-15 00:17:48 +0530

    Enabled CORS for production

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
-//const cors = require("cors");
+const cors = require("cors");
+
+
 
 const app = express();
 
+app.use(cors());
 // parse requests of content-type - application/json
 app.use(express.json());
 
