7c00f410cfb2b674966bc9dceaafa1b4832638da
praveen gowda s r <praveengowdasr998@gmail.com>
2026-02-15 22:54:13 +0530

    Updated baseUrl with new EC2 IP1

--- frontend/src/app/services/tutorial.service.ts
+++ frontend/src/app/services/tutorial.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
 
-const baseUrl = 'http://13.235.55.227:8080/api/tutorials';
+const baseUrl = 'http://13.202.91.108:8080/api/tutorials';
 
 @Injectable({
   providedIn: 'root'
