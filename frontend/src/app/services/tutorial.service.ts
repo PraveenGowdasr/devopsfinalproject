4e754741f094e50857a5469e4caec98321b208bc
praveen gowda s r <praveengowdasr998@gmail.com>
2026-02-24 21:03:52 +0530

    new elstic ip assign

--- frontend/src/app/services/tutorial.service.ts
+++ frontend/src/app/services/tutorial.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
 
-const baseUrl = 'http://13.202.91.108:8080/api/tutorials';
+const baseUrl = 'http://13.234.128.165:8080/api/tutorials';
 
 @Injectable({
   providedIn: 'root'
